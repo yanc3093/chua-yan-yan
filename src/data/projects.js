b84cdafbfac6f29c163d1af19da45bc9eb97c092
yanc3093 <yanc3093@gmail.com>
2025-03-12 21:18:00 +0800

    Implement projects screen

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    id: 1,
+    image: "logo512.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    link: "https://github.com/yanc3093",
+  },
+  {
+    id: 2,
+    image: "logo512.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    link: "https://github.com/yanc3093",
+  },
+  {
+    id: 3,
+    image: "logo512.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    link: "https://github.com/yanc3093",
+  },
+  {
+    id: 4,
+    image: "logo512.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    link: "https://github.com/yanc3093",
+  },
+];
+
+export default projects;
