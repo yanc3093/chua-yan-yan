171da80f4d871ccba32828074adc84a7c9b7f2bf
yanc3093 <yanc3093@gmail.com>
2025-03-13 18:41:37 +0800

    Implement footer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
 import Navbar from "./component/navbar";
 import Home from "./component/home";
 import Resume from "./component/resume";
 import Projects from "./component/projects";
 import Contact from "./component/contact";
+import Footer from "./component/footer";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/resume">
-            <Resume />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
